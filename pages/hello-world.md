b4fa89917bdec48fc176482824765f569a023ab3
Daniel Griffin <dangriff50@gmail.com>
2015-07-13 17:52:42 -0400

    Add simple pages route

---
+++ pages/hello-world.md
@@ -0,0 +1,6 @@
+title: Hello World
+date: 2015-07-13
+
+**Hello World**
+
+This page has been rendered using *Flask* and *Flask FlatPages*.
--- sitebuilder.py
+++ sitebuilder.py
@@ -1,13 +1,29 @@
+# import libraries
 from flask import Flask
+from flask_flatpages import FlatPages
+
+# settings for FlatPages
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+# initialize the application
 app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
 
 # set server name due to development environment restrictions
 SERVER_NAME = "0.0.0.0"
 SERVER_PORT = 8080
 
+# define the routes
 @app.route("/")
 def index():
     return "Hello World"
+
+@app.route('/<path:path>/')
+def page(path):
+    return pages.get_or_404(path).html
 
 if __name__ == "__main__":
     app.run(SERVER_NAME, SERVER_PORT)
